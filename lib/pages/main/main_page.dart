cd387f26b9376acb607a57752e001bedc69391e7
王闯 <wangchuang@ymt360.com>
2019-10-12 16:24:26 +0800

    添加部分注释

--- lib/pages/main/main_page.dart
+++ lib/pages/main/main_page.dart
@@ -76,31 +76,60 @@ class MainPage extends StatelessWidget {
 //              : null,
 //        ),
         body: Container(
-          child: SingleChildScrollView(
+          width: double.infinity, // 宽度占满
+          height: double.infinity, // 高度占满
+          color: Colors.black12,
+          margin: EdgeInsets.all(20), // 外边距
             child: new Column(
+//              https://www.jianshu.com/p/1d003ab6c278
+              mainAxisAlignment: MainAxisAlignment.start, // 主轴布局方式:column主轴方向是垂直的方向
+              crossAxisAlignment: CrossAxisAlignment.start, // 交叉轴的布局方式:column来说就是水平方向的布局方式
+              verticalDirection: VerticalDirection.down, // child的垂直布局方向
+              textDirection: TextDirection.ltr, // 文字放向
               children: <Widget>[
                 new Text('哈哈，这是我最可爱的妹子呢'),
-                new CupertinoButton(child: new Text('点击进入图灵社区'), onPressed: () {
-                  gotoWeb(context);
-                }),
-//                new MaterialButton(child: new Text('打开SQL必知必会PDF'), onPressed: () => Navigator.push(
-//                  context,
-//                  MaterialPageRoute(builder: (context) => SQLKnow()),
-//                )),
+                new CupertinoButton(
+                    padding: EdgeInsets.all(10), // padding 值
+                    color: Colors.amber,
+                    child: new Text('点击进入图灵社区'),
+                    onPressed: () {
+                      gotoWeb(context);
+                    }),
+                new CupertinoButton(
+//                    minSize: 20, // 可以理解为最小宽度
+                    borderRadius: BorderRadius.all(Radius.circular(4.0)), // 圆角 默认8
+                    padding: EdgeInsets.all(10),
+                    disabledColor: Colors.greenAccent,
+                    color: Colors.red,
+                    child: new Text('图灵社区-拥有的书'),
+                    onPressed: () {
+                      gotoWebBook(context);
+                    }),
+                new Container(
+                  margin: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                  child: CupertinoButton.filled(child: new Text('具有外边距的按钮'), onPressed: null),
+                )
+                //                new MaterialButton(child: new Text('打开SQL必知必会PDF'), onPressed: () => Navigator.push(
+                //                  context,
+                //                  MaterialPageRoute(builder: (context) => SQLKnow()),
+                //                )),
               ],
-            )
-          ),
-        ),
+            )),
       ),
     );
   }
 
   gotoWeb(context) {
-    print("点击按钮");
     String path = Uri.encodeComponent('https://www.ituring.com.cn/');
     String title = Uri.encodeComponent('首页-图灵社区');
-    Application.router.navigateTo(context, '${Routes.webViewPage}?title=$title&url=$path');
+    Application.router
+        .navigateTo(context, '${Routes.webViewPage}?title=$title&url=$path');
   }
 
-
+  gotoWebBook(context) {
+    String path = Uri.encodeComponent('https://www.ituring.com.cn/user/shelf');
+    String title = Uri.encodeComponent('拥有的书');
+    Application.router
+        .navigateTo(context, '${Routes.webViewPage}?title=$title&url=$path');
+  }
 }
