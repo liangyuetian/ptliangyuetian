4e2cca5d57742dc3739ac80cde67096966b6ee26
王闯 <wangchuang@ymt360.com>
2019-10-17 09:14:13 +0800

    添加oktoast和fpull_to_refresh插件

---
+++ lib/config/provider_manager.dart
@@ -0,0 +1,3 @@
+import 'package:provider/provider.dart';
+
+List<SingleChildCloneableWidget> providers = [];
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
+import 'package:provider/provider.dart';
+
+import 'package:oktoast/oktoast.dart';
+import 'package:ptliangyuetian/config/provider_manager.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+import 'package:ptliangyuetian/view_model/theme_model.dart';
+import 'package:ptliangyuetian/view_model/locale_model.dart';
+
 import 'package:ptliangyuetian/pages/main/main_page.dart';
 import 'package:ptliangyuetian/router/routers.dart';
 import 'package:ptliangyuetian/router/application.dart';
@@ -20,14 +29,28 @@ class _MyApp extends State {
     Application.router = router;
   }
 
+  Widget app = MaterialApp(
+    title: 'pt liangyuetian',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: new MainPage(),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'pt liangyuetian',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MainPage(),
+    return OKToast(
+        child: MultiProvider(
+          providers: providers,
+          child: Consumer2<ThemeModel, LocaleModel>(
+            builder: (context, themeModel, localeModel, child) {
+              return RefreshConfiguration(
+                hideFooterWhenNotFull: true,
+                child: app,
+              );
+            }
+          )
+        )
     );
   }
 }
