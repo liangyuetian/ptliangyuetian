efd4fbc40de0643ae62bd201e0e5eec94401daf0
pt <ptliangyuetian@gmail.com>
2019-10-19 16:15:02 +0800

    添加home 页

---
+++ lib/provider/provider_widget.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ProviderWidget2<A extends ChangeNotifier, B extends ChangeNotifier>
+    extends StatefulWidget {
+  final Widget Function(BuildContext context, A model1, B model2, Widget child)
+      builder;
+  final A model1;
+  final B model2;
+  final Widget child;
+  final Function(A, B) onModelReady;
+
+  ProviderWidget2({
+    Key key,
+    @required this.builder,
+    @required this.model1,
+    @required this.model2,
+    this.child,
+    this.onModelReady,
+  }) : super(key: key);
+
+  _ProviderWidgetState2<A, B> createState() => _ProviderWidgetState2();
+}
+
+class _ProviderWidgetState2<A extends ChangeNotifier, B extends ChangeNotifier>
+    extends State<ProviderWidget2<A, B>> {
+  A model1;
+  B model2;
+
+  @override
+  void initState() {
+    model1 = widget.model1;
+    model2 = widget.model2;
+
+    if (widget.onModelReady != null) {
+      widget.onModelReady(model1, model2);
+    }
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider<A>(
+            builder: (context) => model1,
+          ),
+          ChangeNotifierProvider<B>(
+            builder: (context) => model2,
+          )
+        ],
+        child: Consumer2<A, B>(
+          builder: widget.builder,
+          child: widget.child,
+        ));
+  }
+}
--- lib/ui/page/tab/home_page.dart
+++ lib/ui/page/tab/home_page.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:provider/provider.dart';
+
+import 'package:ptliangyuetian/config/router_manger.dart';
+
+import 'package:ptliangyuetian/provider/provider_widget.dart';
+import 'package:ptliangyuetian/view_model/home_model.dart';
+import 'package:ptliangyuetian/view_model/scroll_controller_model.dart';
+import 'package:ptliangyuetian/ui/helper/refresh_helper.dart';
+
+const double kHomeRefreshHeight = 180.0;
 
 class HomePage extends StatefulWidget {
 
@@ -14,8 +25,61 @@ class _HomePage extends State<HomePage> with AutomaticKeepAliveClientMixin {
   Widget build(BuildContext context) {
     // TODO: implement build
     super.build(context);
-    return new Center(
-      child: new Text('home page!'),
+    var bannerHeight = MediaQuery
+        .of(context)
+        .size
+        .width * 5 / 11;
+
+    return ProviderWidget2<HomeModel, TapToTopModel>(
+      model1: HomeModel(),
+      model2: TapToTopModel(PrimaryScrollController.of(context),
+          height: bannerHeight - kToolbarHeight),
+      onModelReady: (homeModel, tapToTopModel) {
+        homeModel.initData();
+        tapToTopModel.init();
+      },
+      builder: (context, homeModel, tapToTopModel, child) {
+        return Scaffold(
+            body: MediaQuery.removePadding(
+                context: context,
+                removeTop: false,
+                child: Builder(builder: (_) {
+                  return RefreshConfiguration.copyAncestor(
+                      context: context,
+                      // 下拉触发二楼距离
+                      twiceTriggerDistance: kHomeRefreshHeight - 15,
+                      //最大下拉距离,android默认为0,这里为了触发二楼
+                      maxOverScrollExtent: kHomeRefreshHeight,
+                      child: SmartRefresher(
+                          controller: homeModel.refreshController,
+                          header: HomeRefreshHeader(),
+                          enableTwoLevel: homeModel.idle,
+                          onTwoLevel: () async {
+                            await Navigator.of(context)
+                                .pushNamed(RouteName.homeSecondFloor);
+                            await Future.delayed(Duration(milliseconds: 300));
+                            Provider
+                                .of<HomeModel>(context)
+                                .refreshController
+                                .twoLevelComplete();
+                          },
+                          footer: RefresherFooter(),
+                          enablePullDown: homeModel.idle,
+                          onRefresh: homeModel.refresh,
+                          onLoading: homeModel.loadMore,
+                          enablePullUp: homeModel.list.isNotEmpty,
+                          child: CustomScrollView(
+                              controller: tapToTopModel.scrollController,
+                              slivers: <Widget>[
+                                SliverToBoxAdapter(),
+                              ]
+                          )
+                      )
+                  );
+                })
+            )
+        );
+      },
     );
   }
 }
